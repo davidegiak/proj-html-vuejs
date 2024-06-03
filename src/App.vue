938502db04bf8e7208b3cb7dcdad47ce6907d4fb
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-06-03 19:06:10 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script>
+import AppContent2 from './components/AppContent2.vue'
+export default {
+  components: {
+    AppContent2,
+  }
+}
+</script>
+
+<template>
+  <AppContent2 />
+</template>
---
+++ src/components/AppContent2.vue
@@ -0,0 +1,150 @@
+<script>
+export default {
+  data() {
+
+  }
+}
+</script>
+
+<template>
+  <img class="svg-top" src="/public/img/svg/svg-0.svg" alt="">
+  <div class="background pb-5">
+    <div class="myCont text-center">
+      <img src="/public/img/Clock-and-Bell.png" alt="" style="width: 60px;">
+      <h1 class="round-font">University year</h1>
+      <div class="row justify-content-around position-relative mt-5">
+        <div class="cardof first position-relative">
+          <h4 class="round-font">Demo Classes</h4>
+          <p>In the first week, students try to accommodate with the teaching style and choose their optional courses.</p>
+        </div>
+        <div class="cardof second position-relative">
+          <h4 class="round-font">Demo Classes</h4>
+          <p>In the first week, students try to accommodate with the teaching style and choose their optional courses.</p>
+        </div>
+      </div>
+      <img src="/public/img/Timeline-Item.png" alt="">
+      <div class="row justify-content-around position-relative">
+        <div class="cardof therd position-relative">
+          <h4 class="round-font">Demo Classes</h4>
+          <p>In the first week, students try to accommodate with the teaching style and choose their optional courses.</p>
+        </div>
+        <div class="cardof forth position-relative">
+          <h4 class="round-font">Demo Classes</h4>
+          <p>In the first week, students try to accommodate with the teaching style and choose their optional courses.</p>
+        </div>
+      </div>
+      <img class="mt-4" src="/public/img/upcoming-events-calendar-icon.png" alt="" style="width: 60px;">
+      <h1 class="round-font">Upcoming Events</h1>
+      <div class="row justify-content-center gap-3">
+        <div class="card col-3 p-5 mb-5">
+          <h4 class="round-font">Coaching Session</h4>
+          <p class="lighter-color"><i class="fa-regular fa-calendar-days"></i> 20 May 21:30 PM</p>
+          <p class="lighter-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor[...]</p>
+          <div class="btn-cont">
+            <button type="button" class="button round-font">+ Find more</button>
+          </div>
+        </div>
+        <div class="card col-3 p-5 mb-5">
+          <h4 class="round-font">Coaching Session</h4>
+          <p class="lighter-color"><i class="fa-regular fa-calendar-days"></i> 20 May 21:30 PM</p>
+          <p class="lighter-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor[...]</p>
+          <div class="btn-cont">
+            <button type="button" class="button round-font">+ Find more</button>
+          </div>
+        </div>
+        <div class="card col-3 p-5 mb-5">
+          <h4 class="round-font">Coaching Session</h4>
+          <p class="lighter-color"><i class="fa-regular fa-calendar-days"></i> 20 May 21:30 PM</p>
+          <p class="lighter-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor[...]</p>
+          <div class="btn-cont">
+            <button type="button" class="button round-font">+ Find more</button>
+          </div>
+        </div>
+      </div>
+      <button type="button" class="button-white">View all events</button>
+    </div>
+  </div>
+  <img class="svg" src="/public/img/svg/svg-1.svg" alt="">
+</template>
+
+<style scoped>
+.myCont {
+  width: 1600px;
+  margin: 0 auto;
+}
+
+.background {
+  background-color: #e56768;
+}
+
+.svg {
+  width: 100%;
+  transform: translateY(-5px);
+}
+
+h1 {
+  font-size: 3rem;
+}
+.cardof {
+  width: 25rem;
+}
+.first {
+ left: 18rem;
+}
+.second {
+  right: 4rem;
+}
+.therd {
+  left: 4rem;
+}
+.forth {
+  right: 18rem;
+}
+.lighter-color {
+  opacity: 0.6;
+}
+.card {
+  border-radius: 10px;
+}
+
+.button-white {
+  padding: 16px 43px !important;
+  background-color: #e9d758;
+  margin-left: 20px;
+  border: transparent;
+  color: #fff;
+  border-radius: 32px;
+  font-weight: 600;
+  transition: 0.3s ease;
+  box-shadow: 5px 5px 10px #222;
+}
+.button-white:hover {
+  padding: 16px 43px !important;
+  background-color: transparent;
+  margin-left: 20px;
+  color: #fff;
+  border-radius: 32px;
+  border: transparent;
+  font-weight: 600;
+}
+
+.button {
+  padding: 0.5rem 1rem !important;
+  background-color: #e56768;
+  color: #fff;
+  border-radius: 32px;
+  border: transparent;
+  font-weight: 600;
+  transition: 0.3s ease;
+  width: fit-content;
+  box-shadow: 5px 5px 10px #222;
+}
+
+.button:hover {
+  background-color: #e9d758;
+}
+
+.btn-cont {
+  transform: translateY(4rem);
+}
+</style>
